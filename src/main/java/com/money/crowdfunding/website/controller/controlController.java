93a5749bcd47bf01b3536f35cd67d497ebc59418
wanghongbin <945199413@qq.com>
2021-05-21 17:45:53 +0800

    新项目

---
+++ src/main/java/com/money/crowdfunding/website/controller/controlController.java
@@ -0,0 +1,12 @@
+package com.money.crowdfunding.website.controller;
+
+/**
+ * 包:com.money.crowdfunding.website.controller
+ * 作者:
+ * 日期:
+ * 项目:website
+ * 描述:
+ */
+public class controlController {
+
+}
