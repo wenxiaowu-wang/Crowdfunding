30f1a3b4be6e9cab7e76e5e77b4c232928c6f945
wanghongbin <945199413@qq.com>
2021-05-21 19:00:22 +0800

    新项目

--- src/main/java/com/money/crowdfunding/website/controller/controlController.java
+++ src/main/java/com/money/crowdfunding/website/controller/controlController.java
@@ -1,5 +1,8 @@
 package com.money.crowdfunding.website.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * 包:com.money.crowdfunding.website.controller
  * 作者:
@@ -7,6 +10,18 @@ package com.money.crowdfunding.website.controller;
  * 项目:website
  * 描述:
  */
+@Controller
 public class controlController {
+
+    public controlController(){
+        System.out.println("hello");
+    }
+
+    @RequestMapping("/hello")
+//    @ResponseBody
+    public String sayHello(){
+        System.out.println("进来了");
+        return "html/helloo";
+    }
 
 }
